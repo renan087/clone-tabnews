bc195c00d48fc9ad0aa681908f9ca91b326a28d3
Renan Correia <renan087@gmail.com>
2025-03-19 21:59:42 -0300

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <h1>Estudando programação com Filipe Deschamps</h1>
+      <p>
+        Este é um site de estudo para aprender programação com o Filipe Deschamps.
+      </p>
+    </div>
+  );
 }
-
 export default Home;
