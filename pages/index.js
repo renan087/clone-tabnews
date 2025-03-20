c152ba057d18efa1f5e841b732a8956163e03bf3
Renan Correia <renan087@gmail.com>
2025-03-19 22:07:49 -0300

    alterando a frase novamente

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ function Home() {
       <h1>Estudando programação com Filipe Deschamps</h1>
       <p>
         Este é um site de estudo para aprender programação com o Filipe Deschamps.
+        <a href="https://www.youtube.com/@filipedeschamps">
+          <img src="https://github.com/filipedeschamps.png" alt="Filipe Deschamps" />
+        </a>
       </p>
     </div>
   );
