1b35e1ecff0c885b1749fc026dabbe66c66dbcb0
Renan Correia <renan087@gmail.com>
2025-03-25 21:48:02 -0300

    aidiciona scripts  e

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,21 @@ function Home() {
     <div>
       <h1>Estudando programação com Filipe Deschamps</h1>
       <p>
-        Este é um site de estudo para aprender programação com o Filipe Deschamps.
+        Este é um site de estudo para aprender programação com o Filipe
+        Deschamps.
         <a href="https://www.youtube.com/@filipedeschamps">
-          <img src="https://github.com/filipedeschamps.png" alt="Filipe Deschamps" />
+          <img
+            src="https://github.com/filipedeschamps.png"
+            alt="Filipe Deschamps"
+          />
         </a>
       </p>
     </div>
   );
 }
+
+function Button() {
+  return <button>Clique aqui</button>;
+}
+
 export default Home;
